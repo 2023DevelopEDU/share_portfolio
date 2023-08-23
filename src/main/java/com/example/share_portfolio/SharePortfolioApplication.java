3a7d30fa6f7a8d58c4d17f527e596694f8ac1172
SeongMun-Hwang <hwang105814@gmail.com>
2023-08-23 16:40:38 +0900

    first commit

---
+++ src/main/java/com/example/share_portfolio/SharePortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.example.share_portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharePortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharePortfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/share_portfolio/SharePortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.share_portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharePortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
