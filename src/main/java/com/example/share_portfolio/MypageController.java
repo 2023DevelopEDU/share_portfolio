fad3f31fdef1779aad556429e5b845a161e2318f
seongmun-hwang <hwang105814@gmail.com>
2023-08-28 04:00:09 +0900

    add mypage(only front)

---
+++ src/main/java/com/example/share_portfolio/MypageController.java
@@ -0,0 +1,14 @@
+package com.example.share_portfolio;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/mypage")
+@Controller
+public class MypageController {
+    @GetMapping
+    public String page(){
+        return "my_page";
+    }
+}
