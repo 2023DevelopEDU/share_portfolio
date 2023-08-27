38e3fe3871948a2fe988008a5ff4688debaa0a1e
seongmun-hwang <hwang105814@gmail.com>
2023-08-28 01:46:32 +0900

    add md to html convert

--- src/main/java/com/example/share_portfolio/post/PostController.java
+++ src/main/java/com/example/share_portfolio/post/PostController.java
@@ -2,6 +2,9 @@ package com.example.share_portfolio.post;
 
 import java.util.Optional;
 
+import org.commonmark.node.Node;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,20 +16,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping
 @Controller
 public class PostController {
-    
+
     @Autowired
-    private PostService postService; // Assume you have a service to fetch post data
+    private PostService postService;
 
     @GetMapping(value="post/{id}")
     public String Posts(@PathVariable("id") Long id, Model model){
         Optional<Post> postOptional = postService.getPostById(id);
         if (postOptional.isPresent()) {
-            model.addAttribute("post", postOptional.get());
+            Post post = postOptional.get();
+            Parser parser = Parser.builder().build();
+            Node document = parser.parse(post.getContent());
+            HtmlRenderer renderer = HtmlRenderer.builder().build();
+            String htmlContent = renderer.render(document);
+            post.setContent(htmlContent);
+            model.addAttribute("post", post);
             return "post";
         } else {
             return "404";
         }
     }
+
     @PostMapping("/post/save")
     public String savePost(Post post){
         postService.savePost(post);
