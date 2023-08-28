31214d13cd240d05a12279d4e1587b89481eb9b0
SeongMun-Hwang <hwang105814@gmail.com>
2023-08-28 12:21:30 +0900

    edit navbar&mypage

--- src/main/java/com/example/share_portfolio/MainController.java
+++ src/main/java/com/example/share_portfolio/MainController.java
@@ -1,14 +1,30 @@
 package com.example.share_portfolio;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import com.example.share_portfolio.user.SiteUser;
+import com.example.share_portfolio.user.UserRepository;
 
 @Controller
 public class MainController {
+
+    private final UserRepository userRepository;
+
+    public MainController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @GetMapping(value="/")
-    public String root() {
+    public String root(Model model) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null && auth.isAuthenticated() && !"anonymousUser".equals(auth.getName())) {
+            String username = auth.getName();
+            SiteUser user = userRepository.findByusername(username).orElse(null);
+            model.addAttribute("user", user);
+        }
         return "layout_main";
     }
-    
 }
